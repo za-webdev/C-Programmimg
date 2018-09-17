b2448b20b8c188c415b4425b577a43ad7609de45
Zoya Ali <33239668+za-webdev@users.noreply.github.com>
2018-09-17 16:44:13 -0400

    switch casdes with Csharp

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwithCase
+{
+    class Program
+    {
+        public enum Colors
+        {
+            red,
+            yellow,
+            blue,
+            pink
+        }
+        static void Main(string[] args)
+        {
+            var color = Colors.yellow;
+
+          switch (color)
+            {
+                case Colors.pink:
+                    Console.WriteLine("its pink");
+                    break;
+
+                case Colors.blue:
+                    Console.WriteLine("its blue");
+                    break;
+
+                default:
+                    Console.WriteLine("i dont know");
+                    break;
+
+            }
+
+            
+        }
+    }
+}
