c009f62394bf53bd83954f8a0acec356966161bd
Zoya Ali <33239668+za-webdev@users.noreply.github.com>
2018-09-19 16:26:30 -0400

    learning Enums with C#

---
+++ Example/Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Example.Math;
+
+namespace Example
+{
+    public enum Gender
+    {
+        male,
+        female,
+        unknown
+    }
+    class Person {
+        public string firstname;
+        public string lastname;
+        public Gender gender;
+        public bool isAlive;
+       
+        public void introduce()
+        {
+            var live = (isAlive) ? "alive" : "not alive";
+            Console.WriteLine(string.Format("Name is {0} {1} and gender is {2} and is {3}",firstname,lastname,gender,live));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var me = new Person();
+            me.firstname = "zoya";
+            me.lastname = "ali";
+            me.isAlive = true;
+            me.gender = Gender.female;
+            me.introduce();
+
+        }
+    }
+}
