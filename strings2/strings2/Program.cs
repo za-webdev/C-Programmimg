e7ef1b9faf2984e8e146e4ee5f74255d07408742
Zoya Ali <33239668+za-webdev@users.noreply.github.com>
2018-10-08 09:16:02 -0400

    Some strings

---
+++ strings2/strings2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings2
+{
+    class Program
+    {
+        public static string result;
+        public static string DoSomething(string input)
+        {
+            var result = "";
+            var sentence = input.Split(' ');
+            
+            for (var i = 0; i < sentence.Length; i++)
+            {
+               
+                var str = char.ToUpper(sentence[i][0]) + sentence[i].ToLower().Substring(1);
+                result += str;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a sentence");
+            var input = Console.ReadLine();
+            var x =DoSomething(input);
+            Console.WriteLine(x);
+        }
+
+    }
+}
