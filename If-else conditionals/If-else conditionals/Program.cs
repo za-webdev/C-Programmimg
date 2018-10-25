24bf3843a6737bd5b707164a9900f9fe2bc6395a
Zoya Ali <33239668+za-webdev@users.noreply.github.com>
2018-10-25 13:56:48 -0400

    console app with Unit Testing

---
+++ If-else conditionals/If-else conditionals/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else_conditionals
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 15;
+
+            if( num >=1 && num <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public int Test()
+        {
+            return 25;
+        }
+
+        public string CheckValidity(int num)
+        {
+            if (num >= 1 && num <= 10
+                )
+            {
+                 return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+        }
+    }
+}
---
+++ If-else conditionals/UnitPractice/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using If_else_conditionals;
+
+namespace UnitPractice
+{
+    [TestFixture]
+    public class ProgramTest
+    {
+        private static string Invalid = "Invalid";
+
+        [Test]
+        public void TestMethod_NoArgumnets_Returns25()
+        {
+            Program Myprog = new Program();
+            int MyTestresult = Myprog.Test();
+            Assert.AreEqual(15, MyTestresult);
+        }
+
+        [Test]
+        public void CheckValidity_GreaterThan10_ExpectInvalid()
+        {
+            Program Myprog = new Program();
+            string MyTestresult = Myprog.CheckValidity(11);
+            Assert.AreEqual(Invalid, MyTestresult);
+        }
+
+        [Test]
+        public void CheckValidity_GreaterExactly10_ExpectValid()
+        {
+            Program Myprog = new Program();
+            string MyTestresult = Myprog.CheckValidity(10);
+            Assert.AreEqual(Invalid, MyTestresult);
+        }
+
+
+    }
+}
